47824210979cab2e427aad41be6399ada6358775
sourabh patidar <patidarsourabh89@@example.com>
2024-12-09 11:29:21 +0530

    omdb movies

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import OmdbMovies from './Components/OmdbMovies'
+import Navbar from './Components/navbar/Navbar'
+// import './Components/OmdbMovies.css'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <OmdbMovies/>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/Components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navDiv'>Welcome To <span className='omdbSpan'>OMDB</span> Movies Site</div>
+  )
+}
+
+export default Navbar
